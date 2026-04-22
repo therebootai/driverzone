dc77b2660a026a793edd2b5a7298cf6d5f5c9044
Aniket Saha <sahaaniket720@gmail.com>
2026-04-23 01:42:19 +0530

    feat: add API routes for sending and resending OTPs with user validation logic

--- src/app/api/otp/send/route.ts
+++ src/app/api/otp/send/route.ts
@@ -3,12 +3,41 @@
 import { createOTP } from "@/actions/OTPActions";
 import { SEND_BY_WHATSAPP } from "@/actions/waActions";
 import { ensureModelsRegistered } from "@/db/connection";
+import Driver from "@/models/Drivers";
+import Customer from "@/models/Customers";
+import User from "@/models/Users";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
     const data = await request.json();
     await ensureModelsRegistered();
+
+    if (data.type === "login") {
+      const phone = data.phone;
+      const [driver, customer, user] = await Promise.all([
+        Driver.findOne({ mobile_number: phone }),
+        Customer.findOne({ mobile_number: phone }),
+        User.findOne({ mobile_number: phone }),
+      ]);
+
+      const existingUser = driver || customer || user;
+
+      if (!existingUser) {
+        return NextResponse.json(
+          { message: "number is not registered in database", success: false },
+          { status: 200 }
+        );
+      }
+
+      if (existingUser.status === false) {
+        return NextResponse.json(
+          { message: "Login is not allowed", success: false },
+          { status: 200 }
+        );
+      }
+    }
+
     const otp = await createOTP(data.email, data.phone, data.type);
     console.log(otp);
     await SEND_BY_WHATSAPP({ mobile: data.phone, message: otp });
